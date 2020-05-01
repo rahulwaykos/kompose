871365f12a244f759a9f11623805c42590210f86
rahulwaykos <60727515+rahulwaykos@users.noreply.github.com>
2020-05-01 17:42:59 +0530

    Update kompose.sh

--- kompose.sh
+++ kompose.sh
@@ -1,4 +1,4 @@
-curl -L https://github.com/kubernetes/kompose/releases/download/v1.19.0/kompose-linux-amd64 -o kompose
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.21.0/kompose-linux-amd64 -o kompose
 
 chmod +x kompose
 mv ./kompose /usr/local/bin/kompose
